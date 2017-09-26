be63c6a231577e91ab7eee66dcbf2a5164699058
Brendan LE GLAUNEC <brendan.leglaunec@etixgroup.com>
2017-09-26 09:51:17 +0200

    Add package overview & fix dead link

---
+++ cmrdr.go
@@ -0,0 +1,14 @@
+// Package cmrdr provides methods to be able to discover and
+// attack RTSP streams easily. RTSP streams are used by most
+// IP Cameras, often for surveillance.
+//
+// A simple example usage of the library can be found in
+// https://github.com/EtixLabs/cameradar/tree/master/cameraccess
+//
+// The example usage is complete enough for most users to
+// ignore the library, but for users with specific needs
+// such as creating their own bruteforcing dictionary to
+// access cameras, or running their own network scan, this
+// library allows to use simple and performant methods to
+// attack streams.
+package cmrdr
