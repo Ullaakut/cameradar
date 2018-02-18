e80705c43fbc418056d33f3e23902cd7487ce534
Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>
2018-02-18 19:18:05 +0100

    v2.0.0: Migrate cameradar server to cameradar-app repo

--- server/adaptor/websocket.go
+++
@@ -1,8 +0,0 @@
-package adaptor
-
-// WebSocket is an interface that represents an authenticated websocket connection
-type WebSocket interface {
-	AccessToken() string
-	Read() <-chan interface{}
-	Write() chan<- interface{}
-}
