20daf733711bcddee513f2225ef3946d4b3fb107
Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>
2018-02-18 19:18:05 +0100

    Migrate cameradar server to cameradar-app repo

--- server/adaptor/websocketFactory.go
+++
@@ -1,9 +0,0 @@
-package adaptor
-
-import "net/http"
-
-// WebSocketFactory is an interface for creating generic websocket connections
-type WebSocketFactory interface {
-	NewIncomingWebSocket(w http.ResponseWriter, req *http.Request) (WebSocket, error)
-	NewWebSocket(url string) (WebSocket, error)
-}
